ca72832f6953d59ee925458a8d4ffa3aed9bc16e
Viktor <t@elless.me>
2018-06-08 00:17:55 +0300

    Remove ssl

--- main.go
+++ main.go
@@ -26,19 +26,23 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert(config.Domain + bot.Token, "cert.pem"))
+	_, err = bot.SetWebhook(tgbotapi.NewWebhook(config.Domain + bot.Token))
 	if err != nil {
 		log.Fatal(err)
 	}
 	info, err := bot.GetWebhookInfo()
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	if info.LastErrorDate != 0 {
 		log.Printf("[Telegram callback failed]%s", info.LastErrorMessage)
 	}
+
 	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServeTLS("0.0.0.0:8443", "cert.pem", "key.pem", nil)
+
+	go http.ListenAndServe("0.0.0.0:3000", nil)
 
 	for update := range updates {
 		log.Printf("%+v\n", update)
